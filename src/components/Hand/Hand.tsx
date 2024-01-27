b94d7831cf9e92edfaef35567876e7755559d138
juan pablo <juanpablo.maurer93@gmail.com>
2024-01-27 14:44:57 +0100

    populate hand with dice output

---
+++ src/components/Hand/Hand.tsx
@@ -0,0 +1,42 @@
+// components/Hand/Hand.tsx
+import React from 'react';
+import Tile from '../Tile';
+import { mergeTiles } from '../../utils/gameUtils'; // Assuming this utility is implemented
+import './Hand.css';
+
+type HandProps = {
+  tiles: string[];
+  setTiles: React.Dispatch<React.SetStateAction<string[]>>;
+};
+
+const Hand: React.FC<HandProps> = ({ tiles, setTiles }) => {
+  const handleTileDrop = (targetFigure: string) => (event: React.DragEvent) => {
+    event.preventDefault();
+    const droppedFigure = event.dataTransfer.getData("figure");
+
+    setTiles((prevTiles) => {
+      const newTiles = [...prevTiles];
+      const targetIndex = newTiles.indexOf(targetFigure);
+      if (targetIndex !== -1) {
+        newTiles[targetIndex] = mergeTiles(targetFigure, droppedFigure);
+        return newTiles.filter(tile => tile !== droppedFigure);
+      }
+      return newTiles;
+    });
+  };
+
+  return (
+    <div className="hand">
+      {tiles.map((tile, index) => (
+        <Tile 
+          key={`${tile}-${index}`} 
+          figure={tile}
+          onDragStart={() => {}} // Implement if needed
+          onDrop={handleTileDrop(tile)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Hand;

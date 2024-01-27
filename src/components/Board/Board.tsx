f79c911d7ef49286a5ab46db71645e096b7c06fe
juan pablo <juanpablo.maurer93@gmail.com>
2024-01-27 17:29:25 +0100

    tiles now populate the board correctly

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,45 +1,50 @@
-import React, { useState } from 'react';
-import './Board.css';
+// components/Board/Board.tsx
+import React, { useState } from "react";
+import Tile from "../Tile";
+import { mergeTiles, updateBoard } from "../../utils/gameUtils";
+import "./Board.css";
 
 type BoardState = (string | null)[][];
 
 const Board: React.FC = () => {
   const gridSize = 4;
-  const initialBoardState: BoardState = Array.from({ length: gridSize }, () => Array(gridSize).fill(null));
+  const initialBoardState: BoardState = Array.from({ length: gridSize }, () =>
+    Array(gridSize).fill(null)
+  );
   const [board, setBoard] = useState<BoardState>(initialBoardState);
 
-  const handleDrop = (event: React.DragEvent<HTMLDivElement>, row: number, col: number) => {
+  const handleDrop = (row: number, col: number) => (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
-    const figure = event.dataTransfer.getData("figure");
-
-    // Update the board state with the new figure
-    setBoard(currentBoard => {
-      // Clone the current board
-      const newBoard = currentBoard.map(innerArray => innerArray.slice());
-      // Update the specific cell
-      newBoard[row][col] = figure;
-      return newBoard;
-    });
-  };
+    const figure = event.dataTransfer.getData("text/plain");
+      // Update the board state with the new or merged figure
+      setBoard((currentBoard) => {
+        const targetCell = currentBoard[row][col];
+        const newFigure = targetCell ? mergeTiles(targetCell, figure) : figure;
+        return updateBoard(currentBoard, row, col, newFigure);
+      });
+    };
 
   const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault(); // Necessary to allow dropping
+    event.preventDefault();
   };
 
   return (
     <div className="board">
-      {board.map((row: (string | null)[], rowIndex: number) =>
-        row.map((cell: string | null, colIndex: number) => (
-          <div 
-            key={`${rowIndex}-${colIndex}`} 
-            className="board-cell" 
-            onDrop={(e) => handleDrop(e, rowIndex, colIndex)}
+      {board.map((row, rowIndex) => {
+        console.log("Row: ", row); // Debugging
+        return row.map((cell, colIndex) => (
+          <div
+            key={`${rowIndex}-${colIndex}`}
+            className="board-cell"
+            onDrop={handleDrop(rowIndex, colIndex)}
             onDragOver={handleDragOver}
           >
-            {cell}
+            {cell && (
+              <Tile figure={cell} onDragStart={() => {}} onDrop={() => {}} />
+            )}
           </div>
-        ))
-      )}
+        ));
+      })}
     </div>
   );
 };

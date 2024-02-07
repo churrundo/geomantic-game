7853065eec559837a5fb07c95e422d02e32e5b98
juan pablo <juanpablo.maurer93@gmail.com>
2024-02-07 14:50:26 +0100

    restyle for clarity

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
+// App.tsx
 import React, { useCallback } from "react";
 import Board from "./components/Board";
 import Hand from "./components/Hand";
 import Dice from "./components/Dice";
 import { useGameContext } from "./GameContext";
+import "./App.css"
 
 const App = () => {
   const { state, dispatch } = useGameContext();
-  const { boardTiles, currentPlayer, player1Hand, player2Hand } = state;
-  const currentHand = currentPlayer === "player1" ? player1Hand : player2Hand;
+  const { boardTiles, player1Hand, player2Hand, currentPlayer } = state;
 
   const handleDiceRoll = useCallback((figures: string[]) => {
-    // Dispatch an action to update the hand tiles in the global state
     dispatch({ type: "PLAYER_DICE_ROLL", payload: { player: currentPlayer, newHand: figures } });
-    // eslint-disable-next-line
-  }, [currentPlayer]); // Only currentPlayer is the dependency
+  }, [dispatch, currentPlayer]);
 
   return (
-      <div className="App">
-        <h1>Geomantic Game</h1>
-        <Board boardTiles={boardTiles} />
-        <Dice onRoll={handleDiceRoll} />
-        <Hand tiles={currentHand} />
-      </div>
+    <div className="App">
+  <h1>Geomantic Game</h1>
+  <div className="game-area">
+    <Hand tiles={player1Hand} player="player1" currentPlayer={currentPlayer} />
+    <Board boardTiles={boardTiles} />
+    <Hand tiles={player2Hand} player="player2" currentPlayer={currentPlayer} />
+  </div>
+  <Dice onRoll={handleDiceRoll} />
+</div>
+
   );
 };
 
